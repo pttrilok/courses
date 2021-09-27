ab58dfe1c65d5a9739eee69ae8039f2fb3294411
teju0 <76488943+tejus0@users.noreply.github.com>
2021-09-26 23:39:40 -0700

    Update AliceAndStrings.cpp

--- Hackerearth/Algorithms/String Algorithms/Basics of String Manipulation/AliceAndStrings.cpp
+++ Hackerearth/Algorithms/String Algorithms/Basics of String Manipulation/AliceAndStrings.cpp
@@ -1,3 +1,6 @@
+QUESTION -> https://www.hackerearth.com/practice/algorithms/string-algorithm/basics-of-string-manipulation/practice-problems/algorithm/aliceandstrings-9da62aa7/
+
+
 #include<bits/stdc++.h>
 using namespace std;
 int main()
