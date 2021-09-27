b57f0e9470a87bdef3a8eee9e3e6671207f6c727
teju0 <76488943+tejus0@users.noreply.github.com>
2021-09-27 13:37:37 +0530

    EASY

---
+++ Hackerearth/Algorithms/String Algorithms/Basics of String Manipulation/Rotation.cpp
@@ -0,0 +1,50 @@
+//QUESTION ->  https://www.hackerearth.com/practice/algorithms/string-algorithm/basics-of-string-manipulation/practice-problems/algorithm/rotation-1-38ecf5a7/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+string conv(string a)
+
+{
+
+    a = a.substr(1,a.length());
+
+    return a;
+
+}
+
+int getRes(string a, string b, int n)
+
+{
+
+    for(int i = 0; i < n; i++)
+
+    if(a.substr(0,a.length()) != b.substr(0,a.length())) a = conv(a);
+
+    else break;
+
+    
+
+    return n - a.length();
+
+}
+
+int main()
+
+{
+
+    int n; cin>>n;
+
+    string a, b; cin>>a>>b;
+
+
+ 
+
+    cout<<getRes(a,b,n)<<endl;
+
+    
+
+    return 0;
+
+}
