054a89ba16390a0c97b4675794387a9de0856100
teju0 <76488943+tejus0@users.noreply.github.com>
2021-09-27 12:00:38 +0530

    EASY

---
+++ Hackerearth/Algorithms/String Algorithms/Basics of String Manipulation/AliceAndStrings.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string a,b;
+    cin>>a>>b;
+    int m=a.size();
+    int n=b.size();
+	int flag=0;
+    if(m!=n)
+    {
+        cout<<"NO";
+    }
+    else
+    {
+	for(int i=0;i<m;i++)
+        {
+            if(a[i]>b[i] || a[i]=='z')
+ 
+            {
+ 
+                flag=1;
+ 
+                cout<<"NO";
+ 
+                break;
+ 
+            }
+ 
+        }
+ 
+        if(flag==0)
+ 
+         cout<<"YES";
+ 
+    }
+ 
+    return 0;
+ 
+}
