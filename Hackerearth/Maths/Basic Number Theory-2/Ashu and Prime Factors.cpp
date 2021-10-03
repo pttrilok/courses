80565c0db1904b4c9ca9f088b12a1ab741d42c5a
Vasu-kaushik <91519221+Vasu-kaushik@users.noreply.github.com>
2021-10-03 16:01:57 +0530

    Uploaded by Vasu

---
+++ Hackerearth/Maths/Basic Number Theory-2/Ashu and Prime Factors.cpp
@@ -0,0 +1,34 @@
+//https://www.hackerearth.com/practice/math/number-theory/basic-number-theory-2/practice-problems/algorithm/ashu-and-prime-factors-4/
+
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int prime[1000001]={0};
+  int count[1000001]={0};
+    
+  prime[1]=1;
+   for(int i=2;i<=1000000;i++){
+       if(prime[i]==0){
+           
+           for(int j=i*2;j<=1000000;j+=i){
+               if(prime[j]==0){
+               count[i]++;
+               prime[j]=1;
+               
+               }
+           }
+       }
+   }
+    
+  int t;
+  cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        cout<<count[n]+1<<endl;
+    }
+    
+}
