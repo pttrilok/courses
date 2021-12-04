d6776ce009811c9d5fe415f8d04a1c57be98761a
Vasu-kaushik <91519221+Vasu-kaushik@users.noreply.github.com>
2021-12-04 15:05:17 +0530

    Merge the groups

---
+++ Hackerearth/Data Structure/Disjoint Data Structures/Merge the groups.cpp
@@ -0,0 +1,70 @@
+//https://www.hackerearth.com/practice/data-structures/disjoint-data-strutures/basics-of-disjoint-data-structures/practice-problems/algorithm/merge-the-boxes-7fb988ac/
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+ll parent[1000001];
+ll size[1000001];
+ll gmin[1000001];
+ll gmax[1000001];
+ll arr[1000001];
+
+void init(ll n){
+    for(ll i = 1; i <= n; i++){
+        parent[i] = i;
+        size[i] = 1;
+        gmin[i] = arr[i];
+        gmax[i] = arr[i];
+    }
+}
+
+ll find(ll A){
+    if(parent[A]==A)
+    return A;
+
+    return parent[A]=find(parent[A]);
+}
+
+void unio(ll A, ll B){
+    ll rootA = find(A);
+    ll rootB = find(B);
+    if(size[rootA] < size[rootB]){
+        parent[rootA] = parent[rootB];
+        size[rootB] += size[rootA];
+        gmax[rootB] = max(gmax[rootB], gmax[rootA]);
+        gmin[rootB] = min(gmin[rootB], gmin[rootA]);
+    }
+    else{
+        parent[rootB] = parent[rootA];
+        size[rootA] += size[rootB];
+        gmax[rootA] = max(gmax[rootB], gmax[rootA]);
+        gmin[rootA] = min(gmin[rootB], gmin[rootA]);
+    }
+}
+
+int main(){
+    ll n, i;
+    cin>>n;
+    for(i = 1; i <= n; i++){
+        cin>>arr[i];
+    }
+    init(n);
+    ll q;
+    cin>>q;
+    while(q--){
+        ll x, y;
+        cin>>x>>y;
+        if(x==1){
+            ll z;
+            cin>>z;
+
+            unio(y,z);
+        }
+        else if(x==2){
+            cout<<gmax[find(y)]-gmin[find(y)]<<endl;
+        }
+        
+    }
+}
+
